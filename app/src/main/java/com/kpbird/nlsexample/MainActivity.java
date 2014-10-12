70f5763e02dd550cc615c76454148d129ab5a8b7
technocrat313 <bmm@technocrat313.com>
2014-10-11 23:25:04 -0400

    Initial commit

---
+++ app/src/main/java/com/kpbird/nlsexample/MainActivity.java
@@ -0,0 +1,122 @@
+package com.kpbird.nlsexample;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView txtView;
+    private NotificationReceiver nReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtView = (TextView) findViewById(R.id.textView);
+        nReceiver = new NotificationReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        registerReceiver(nReceiver,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nReceiver);
+    }
+
+
+
+    public void buttonClicked(View v){
+
+        if(v.getId() == R.id.btnCreateNotify){
+            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
+            ncomp.setContentTitle("My Notification");
+            ncomp.setContentText("Notification Listener Service Example");
+            ncomp.setTicker("Notification Listener Service Example");
+            ncomp.setSmallIcon(R.drawable.ic_launcher);
+            ncomp.setAutoCancel(true);
+            nManager.notify((int)System.currentTimeMillis(),ncomp.build());
+        }
+        else if(v.getId() == R.id.btnClearNotify){
+            Intent i = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","clearall");
+            sendBroadcast(i);
+        }
+        else if(v.getId() == R.id.btnListNotify){
+            Intent i = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","list");
+            sendBroadcast(i);
+        }
+
+
+    }
+
+    class NotificationReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
+            txtView.setText(temp);
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//package com.kpbird.nlsexample;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+//
+//public class MainActivity extends Activity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
